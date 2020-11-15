9677b1ab86ab482c8da7d32e9bc1800e799d15d9
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-11-15 13:38:29 -0600

    test: fix ignore dist

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   setupFilesAfterEnv: ["<rootDir>/src/setup.ts"],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
 };
 
 export default config;
