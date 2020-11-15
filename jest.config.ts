965d2fd6da6a1bd13e2ab7a89cd9337755c3d3c0
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-11-15 13:01:44 -0600

    test: fix jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  setupFilesAfterEnv: ["<rootDir>/src/jestSetup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/setup.ts"],
 };
 
 export default config;
